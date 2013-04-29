389d568872f63bf72c3488c29c4e128762f5b6aa
samuelololol <samuelololol@gmail.com>
2013-04-30 02:35:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Walking in Holden's Footsteps
+
+
+Inspired by [NYTimes, Walking in Holden's Footsteps](http://www.nytimes.com/interactive/2010/01/28/nyregion/20100128-salinger-map.html), using Google Map JavaScript API v3 [example](https://developers.google.com/maps/documentation/javascript/examples/icon-complex).
---
+++ location.js
@@ -0,0 +1,46 @@
+var map_location = {
+  x : 40.760,
+  y : -73.969
+};
+
+var points = [];
+
+points.push({page:80, x:40.744006, y:-73.995581, desc:
+"I went down to the elevator again and got a cab and told the driver to take me down to Ernie's. Ernie's is this nightcloub in Greenwich Village that my brother D.B used to go to quite frequently before he went out to Hollywood and prostituted himself." 
+});
+
+points.push({page:59, x:40.7500, y:-73.9923, desc:
+"The first thing I did when I got off at Penn Station, I went into this phone booth. I felt like giving somebody a buzz. I left my bags right outside the booth so that I could watch them, but as soon as I was inside, I couldn't think of anybody to call up."
+});
+
+points.push({page:196, x:40.75009, y:-73.98349, desc:
+"I didn't eat the doughnuts. I couldn't swallow them too well. The thing is, if you get very depressed about something, it's hard as hell to swallow. The waiter was very nice, though. He took them back without charging me. I just drank the coffee. Then I left and started walking over toward Fifth Avenue."
+});
+
+points.push({page:114, x:40.75512, y:-73.9865, desc:
+"I started walking over toward Broadway, just for the hell of it, because I hadn't been over there in years. Besides, I wanted to find a record store that was open on Sunday. There was this record I wanted to get for Phoebe, called \"Little Shirley Beans.\" It was a very hard record to get."
+});
+
+points.push({page:107, x:40.7523,  y:-73.9776, desc:
+  "It was only Sunday, and I couldn't go home till Wednesday--or Tuesday the soonest. And I certainly didn't feel like going to another hotel and getting my brains beat out. So what I did, I told the driver to take me to Grand Central Station. It was right near the Biltmore, where I was meeting Sally later, and I figured what I'd do, I'd check my bags in one of those strong boxes that they give you a key to, then get some breakfast. I was sort of hungry."
+});
+
+points.push({page:194, x:40.7523,  y:-73.9776, desc:
+"I didn't know where the hell to go. I didn't want to go to another hotel and spend all Phoebe's dough. So finally all I did was I walked over to Lexington and took the subway down to Grand Central. My bags were there and all, and I figured I'd sleep in that crazy waiting room where all the benches are. So that's what I did."
+});
+
+points.push({page:196, x:40.7510,  y:-73.9731, desc:
+"I figured I ought to get some breakfast. I wasn't at all hungry, but I figured I ought to at least eat something. I mean at least get something with some vitamins in it. So I started walking way over east, where the pretty cheap restaurants are, because I didn't want to spend a lot of dough."
+});
+
+points.push({page:122, x:40.7545,  y:-73.9760, desc:
+"Then a funny thing happened. When I got to the museum, all of a sudden I wouldn't have gone inside for a million bucks. It just didn't appeal to me--and here I'd walked through the whole goddam park and looked forward to it and all. If Phoebe'd been there, I probably would have, but she wasn't. So all I did, in front of the museum, was get a cab and go down to the Biltmore."
+});
+
+points.push({page:125, x:40.75823, y:-73.98560, desc:
+"We horsed around a little bit in the cab on the way over to the theater. At first she didn't want to, because she had her lipstick on and all, but I was being seductive as hell and she didn't have any alternative. Twice, when the goddam cab stopped short in traffic, I damn near fell off the seat. Those damn drivers never even look where they're going, I swear they don't. Then, just to show you how crazy I am, when we were coming out of this big clinch, I told her I loved her and all. It was a lie, of course, but the thing is, I meant it when I said it. I'm crazy. I swear to God I am."
+});
+
+points.push({page:128, x:40.75855, y:-73.97897, desc:
+"Let's go ice-skating at Radio City!"
+});
